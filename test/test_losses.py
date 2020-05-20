920cd8f85569491e4eac9121628afa36af6973b5
Aayush Naik <aayushnaik17@gmail.com>
2020-05-20 16:07:03 -0700

    Update loss tests

--- test/test_losses.py
+++ test/test_losses.py
@@ -6,27 +6,27 @@ from crar.losses import compute_disambiguation
 class TestDisambiguation:
     def test_with_zero_1d(self):
         a, b = torch.tensor([[0.0]]), torch.tensor([[0.0]])
-        expected = torch.exp(torch.tensor([[-5.0 * 1e-3]]))
+        expected = torch.exp(-5.0 * torch.tensor([[1e-3]]))
         actual = compute_disambiguation(a, b)
-        assert actual == expected
+        assert torch.isclose(expected, actual)
 
     def test_with_zero_2d(self):
         a, b = torch.tensor([[0.0, 0.0]]), torch.tensor([[0.0, 0.0]])
-        expected = torch.exp(torch.tensor([[-5.0 * 1e-3]]))
+        expected = torch.exp(-5.0 * torch.tensor([[1e-3]]))
         actual = compute_disambiguation(a, b)
-        assert actual == expected
+        assert torch.isclose(expected, actual)
 
     def test_with_constant_difference(self):
         a, b = torch.tensor([[0.0]]), torch.tensor([[2.0]])
-        expected = torch.exp(torch.tensor([[-5.0 * 2.0]]))
+        expected = torch.exp(-5.0 * torch.tensor([[2.0]]))
         actual = compute_disambiguation(a, b)
-        assert actual == expected
+        assert torch.isclose(expected, actual)
 
     def test_large_value(self):
         a, b = torch.tensor([[0.0]]), torch.tensor([[100.0]])
         expected = torch.exp(-5.0 * torch.tensor([[np.sqrt(10.0)]]))
         actual = compute_disambiguation(a, b)
-        assert actual == expected
+        assert torch.isclose(expected, actual)
 
     def test_2d_tensor(self):
         a = np.array([[0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
@@ -37,4 +37,4 @@ class TestDisambiguation:
 
         a, b = torch.tensor(a), torch.tensor(b)
         actual = compute_disambiguation(a, b)
-        assert expected == actual
+        assert torch.isclose(expected, actual)
