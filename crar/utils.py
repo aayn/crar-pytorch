4cca8e6302522688186c214ff02c3bdab5f3aaa4
Aayush Naik <aayushnaik17@gmail.com>
2020-05-31 11:35:17 -0700

    Move timing from perf to utils

--- crar/utils.py
+++ crar/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 import torch
+from functools import wraps
+from time import time
 import gym
 import torch.optim as optim
 from itertools import accumulate
@@ -19,3 +21,15 @@ def nonthrowing_issubclass(cl1, cl2) -> bool:
 def compute_eps(current_frame, eps_start=1.0, eps_end=0.0, eps_last_frame=float("inf")):
     eps = max(eps_end, eps_start - (current_frame / eps_last_frame))
     return eps
+
+
+def timeit(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start = time()
+        ret = f(*args, **kwargs)
+        end = time()
+        print(f"Func {f.__name__} took {end - start:.5f} sec")
+        return ret
+
+    return wrapper
